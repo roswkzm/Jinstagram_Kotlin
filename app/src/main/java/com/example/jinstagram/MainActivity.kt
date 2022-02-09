5fd54b9a5e18fe46a2dfa8dae50d216cbc627132
이진영 <roswkzm@naver.com>
2022-02-09 19:42:10 +0900

    firestore에 저장되어있는 정보를 토대로 UserFragment에 정보매핑 RecyclerView 이용하여 포스팅 갯수, 팔로워, 팔로잉 수, RecyclerView로 내가올린 게시물사진 확인

--- app/src/main/java/com/example/jinstagram/MainActivity.kt
+++ app/src/main/java/com/example/jinstagram/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.core.content.ContextCompat
 import com.example.jinstagram.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
@@ -50,6 +51,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             R.id.action_account -> {
                 var userFragment = UserFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid", uid)
+                userFragment.arguments = bundle     // userFragment로 uid값 전송
                 supportFragmentManager.beginTransaction().replace(R.id.main_content, userFragment).commit()
                 return true
             }
