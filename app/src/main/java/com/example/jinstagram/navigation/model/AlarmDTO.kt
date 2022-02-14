bd323d149dcadcbc26ba7c34420a870c39d92924
이진영 <roswkzm@naver.com>
2022-02-14 14:54:28 +0900

    AlarmFragment.kt 에 알람에 대한 모든 정보 저장 후 RecyclerView로 정보 출력

--- app/src/main/java/com/example/jinstagram/navigation/model/AlarmDTO.kt
+++ app/src/main/java/com/example/jinstagram/navigation/model/AlarmDTO.kt
@@ -5,6 +5,9 @@ data class AlarmDTO(
     var userId : String? = null,
     var uid : String? = null,
     var kind : Int? = null,
+    // 0 -> 좋아요 알람
+    // 1 -> 댓글 알람
+    // 2 -> 팔로우 알람
     var message : String? = null,
     var timestamp : Long? = null
 )
